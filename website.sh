65e58cc3a78703a25277c9bc71bc079ca5f07ac3
Jessica <jtstodola@gmail.com>
2015-07-13 20:40:28 -0500

    First Commit

---
+++ website.sh
@@ -0,0 +1,31 @@
+cd ~/Development/websites
+echo "What is the folder name for the website?"
+read folder_name
+mkdir $folder_name
+
+cd $folder_name
+touch index.html
+
+echo "<!DOCTYPE html>
+<html lang=\"en\">
+  <head>
+    <title></title>
+    <meta charset=\"utf\">
+	<!--[if lt IE 9]>
+          <script src=\"http://html5shiv.googlecode.com/svn/trunk/html5.js\">
+	</script>
+    <![endif]-->
+  </head>
+
+  <body>
+
+  </body>
+</html>" > index.html
+
+
+mkdir css
+cd css
+touch styles.css
+cd ../
+
+mkdir js
